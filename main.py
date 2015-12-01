a35814c2e7ebb7a17d938fdd8b54894a606d6777
Nasr Maswood <nasrmaswood@gmail.com>
2015-11-30 23:20:18 -0600

    removed empty lines

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from Map import Map
 from Bubble import Bubble
 
-
-
-
 if __name__ == "__main__":
 
     map = Map()
     bubble = Bubble()
-
 
     map.create_map("heat")
     map.create_map("marker")
